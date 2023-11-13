c25e887fa73e3f6a3a531573b20b0d424501ad13
Joshua <joshua.rawlings@outlook.com>
2023-11-13 16:57:28 +0000

    additional features added

--- eda.py
+++ eda.py
@@ -1,7 +1,7 @@
 #  1. Define problem: Binary classifier model that predicts smoker status. Smoker = 1.
 
 #  2. Collect Data: Data collated by Kaggle as part of competition. No further data sourced.
-
+import numpy as np
 import pandas as pd
 
 # Load training data into a pandas DataFrame
@@ -40,4 +40,31 @@ a = summary(train)
 
 #  4. EdA and Feature Engineering
 
-#  Compute Additional Features
+#  Variables are relatively self-explanatory in how they might affect smoking. i.e. higher risk factors, higher blood pressure/ higher chloestrol etc.
+#  So will focus on creating additional features based on data we have.
+
+# BMI - combines height and weight. Obesity predictor
+train['bmi'] = train['weight(kg)']/(train['height(cm)']/100*train['height(cm)']/100)
+
+# Waist to height ratio. Additional predictor of obesity
+train['wst_hgt'] = train['waist(cm)'] / train['height(cm)']
+
+#  Blood pressure - relevant for systolic #0 - low #1 - normal #2pre-high #4-high
+train['bld_pres'] = pd.cut(train['systolic'], bins=[0, 90, 120, 140, np.inf], labels=False, right=False)
+
+# Diabetic - relevant to the fasting blood sugar
+train['diabetic'] = pd.cut(train['fasting blood sugar'], bins=[0, 99, 125, np.inf], labels=False, right=False)
+
+#  Cholesterol risk factors
+train['tc'] = train['HDL'] + train['LDL'] + 0.2*train['triglyceride']
+train['tc'] = pd.cut(train['tc'], bins=[0, 200, 239, np.inf], labels=False, right=False)
+train['ldl'] = pd.cut(train['LDL'], bins=[0, 129, 159, np.inf], labels=False, right=False)
+train['hdl'] = pd.cut(train['HDL'], bins=[0, 39, 49, np.inf], labels=False, right=False)
+train['trig'] = pd.cut(train['triglyceride'], bins=[0, 200, 399, np.inf], labels=False, right=False)
+
+# Risk factors related to hemoglobin
+train['red_blood'] = pd.cut(train['hemoglobin'], bins=[0, 12, 17.5, np.inf], labels=False, right=False)
+
+#  Kidney Risk factor related to urine protein and serum creatinine
+# Liver risk factors AST, ALT, Gtp
+
